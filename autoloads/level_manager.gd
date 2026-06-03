70310be86138418bf6cdb15025baec32562a839e
Yolekoda <56722206+ilKogan@users.noreply.github.com>
2026-06-03 15:27:04 +0300

    Add 5 level

--- autoloads/level_manager.gd
+++ autoloads/level_manager.gd
@@ -13,34 +13,34 @@ var anim_duration: float = 1.0
 var initial_zoom: float = 1 / scale ** 3
 
 func _ready() -> void:
-    for i in range(1, num_of_levels + 1):
-        levels.append("Level" + str(i))
+	for i in range(1, num_of_levels + 1):
+		levels.append("Level" + str(i))
 
-    camera = Camera2D.new()
-    camera.zoom = Vector2(initial_zoom, initial_zoom)  # Start zoomed in
-    camera.position = get_viewport().size / (2 * initial_zoom)
-    add_child(camera)
-    camera.make_current()
+	camera = Camera2D.new()
+	camera.zoom = Vector2(initial_zoom, initial_zoom)  # Start zoomed in
+	camera.position = get_viewport().size / (2 * initial_zoom)
+	add_child(camera)
+	camera.make_current()
 
 
 func go_to_next_level() -> void:
-    index += 1
+	index += 1
 
-    var target_zoom = camera.zoom.x * scale
-    var tween = camera.create_tween()
-    tween.tween_property(camera, "zoom", Vector2(target_zoom, target_zoom), anim_duration)
-    tween.parallel().tween_property(camera, "position", camera.position / scale, anim_duration)
+	var target_zoom = camera.zoom.x * scale
+	var tween = camera.create_tween()
+	tween.tween_property(camera, "zoom", Vector2(target_zoom, target_zoom), anim_duration)
+	tween.parallel().tween_property(camera, "position", camera.position / scale, anim_duration)
 
-    level_changed.emit(levels[index - 1], levels[index])
+	level_changed.emit(levels[index - 1], levels[index])
 
 
 func go_to_prev_level() -> void:
-    index -= 1
+	index -= 1
 
-    var target_zoom = camera.zoom.x / scale
-    var tween = camera.create_tween()
-    tween.tween_property(camera, "zoom", Vector2(target_zoom, target_zoom), anim_duration)
-    tween.parallel().tween_property(camera, "position", camera.position * scale, anim_duration)
-    await tween.finished
+	var target_zoom = camera.zoom.x / scale
+	var tween = camera.create_tween()
+	tween.tween_property(camera, "zoom", Vector2(target_zoom, target_zoom), anim_duration)
+	tween.parallel().tween_property(camera, "position", camera.position * scale, anim_duration)
+	await tween.finished
 
-    level_changed.emit(levels[index], levels[index + 1])
+	level_changed.emit(levels[index], levels[index + 1])
