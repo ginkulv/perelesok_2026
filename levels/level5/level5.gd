bdaa19237b7b4f5beeb7fd09b8af818c93cfb14f
Yolekoda <56722206+ilKogan@users.noreply.github.com>
2026-06-03 14:44:09 +0300

    Прозрачность и текстовик на раб столе

--- levels/level5/level5.gd
+++ levels/level5/level5.gd
@@ -1,1 +1,24 @@
 extends Node2D
+@export var last_message_text: String
+
+func _ready() -> void:
+	set_transparent_window()
+	create_text_file_on_desktop(last_message_text)
+
+func set_transparent_window()  -> void:
+	get_viewport().transparent_bg = true
+	var game_window = get_viewport().get_window()
+	game_window.transparent = true
+
+
+func create_text_file_on_desktop(text :String) -> void:
+	var desktop_path = OS.get_system_dir(OS.SYSTEM_DIR_DESKTOP)
+	var file_path = desktop_path + "/message.txt"
+	
+	var file = FileAccess.open(file_path, FileAccess.WRITE)
+	if file:
+		file.store_string(last_message_text)
+		file.close()
+		print("Файл создан: ", file_path)
+	else:
+		print("Не удалось создать файл")
