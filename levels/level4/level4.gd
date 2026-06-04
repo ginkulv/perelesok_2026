212154ff1c874812dff017bb14f95481b9859356
Yolekoda <56722206+ilKogan@users.noreply.github.com>
2026-06-04 10:56:56 +0300

    Добавил пыль

--- levels/level4/level4.gd
+++ levels/level4/level4.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
 func _ready() -> void:
-    LevelManager.level_changed.connect(_on_level_changed)
+	LevelManager.level_changed.connect(_on_level_changed)
 
 func _on_level_changed(level_from: String, level_to: String) -> void:
-    if level_to == self.name:
-        CursorManager.invert_x = true
-        $NoteArea2D.dragged_to_the_end.connect(_on_note_dragged_to_the_end)
-        $CarpetArea2D.item_clicked.connect(_on_carpet_clicked)
-        print(level_from)
-    elif level_from == self.name:
-        CursorManager.invert_x = false
+	if level_to == self.name:
+		CursorManager.invert_x = true
+		$NoteArea2D.dragged_to_the_end.connect(_on_note_dragged_to_the_end)
+		$CarpetArea2D.item_clicked.connect(_on_carpet_clicked)
+		print(level_from)
+	elif level_from == self.name:
+		CursorManager.invert_x = false
 
 func _on_note_dragged_to_the_end() -> void:
-    print("Note dragged to the end, some logic happens, good!")
+	print("Note dragged to the end, some logic happens, good!")
 
 func _on_carpet_clicked() -> void:
-    print("Carpet clicked, good!")
+	print("Carpet clicked, good!")
