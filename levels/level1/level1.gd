7d2adb5ffe0cf3b480a3660807195a61e5055a2e
Aske <klaus.aske@yandex.ru>
2026-06-04 11:15:02 +0300

    Чуть поправил код

--- levels/level1/level1.gd
+++ levels/level1/level1.gd
@@ -1,28 +1,28 @@
 extends Node
 
 @onready var expected_pos = {
-	$JigsawFragment2.name: Vector2(766, 212),
-	$JigsawFragment3.name: Vector2(306, 198),
-	$JigsawFragment4.name: Vector2(1219,832),
-	$JigsawFragment5.name: Vector2(1509, 383),
-	$JigsawFragment6.name: Vector2(790, 547),
-	$JigsawFragment7.name: Vector2(1390, 547),
-	$JigsawFragment8.name: Vector2(239, 668),
-	$JigsawFragment9.name: Vector2(496, 404),
+    $JigsawFragment2.name: Vector2(766, 212),
+    $JigsawFragment3.name: Vector2(306, 198),
+    $JigsawFragment4.name: Vector2(1219,832),
+    $JigsawFragment5.name: Vector2(1509, 383),
+    $JigsawFragment6.name: Vector2(790, 547),
+    $JigsawFragment7.name: Vector2(1390, 547),
+    $JigsawFragment8.name: Vector2(239, 668),
+    $JigsawFragment9.name: Vector2(496, 404),
 }
 
 func _ready() -> void:
     MessageManager.message_shown.connect(_on_message_shown)
 
-	$JigsawFragment2.drag_ended.connect(_on_jigsaw_placed)
-	$JigsawFragment3.drag_ended.connect(_on_jigsaw_placed)
-	$JigsawFragment4.drag_ended.connect(_on_jigsaw_placed)
-	$JigsawFragment5.drag_ended.connect(_on_jigsaw_placed)
-	$JigsawFragment6.drag_ended.connect(_on_jigsaw_placed)
-	$JigsawFragment7.drag_ended.connect(_on_jigsaw_placed)
-	$JigsawFragment8.drag_ended.connect(_on_jigsaw_placed)
-	$JigsawFragment9.drag_ended.connect(_on_jigsaw_placed)
-	LevelManager.level_changed.connect(_on_level_changed)
+    $JigsawFragment2.drag_ended.connect(_on_jigsaw_placed)
+    $JigsawFragment3.drag_ended.connect(_on_jigsaw_placed)
+    $JigsawFragment4.drag_ended.connect(_on_jigsaw_placed)
+    $JigsawFragment5.drag_ended.connect(_on_jigsaw_placed)
+    $JigsawFragment6.drag_ended.connect(_on_jigsaw_placed)
+    $JigsawFragment7.drag_ended.connect(_on_jigsaw_placed)
+    $JigsawFragment8.drag_ended.connect(_on_jigsaw_placed)
+    $JigsawFragment9.drag_ended.connect(_on_jigsaw_placed)
+    LevelManager.level_changed.connect(_on_level_changed)
 
 
 func _on_level_changed(level_from: String, level_to: String) -> void:
