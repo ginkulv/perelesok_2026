2cdc0204a1c5e82a659004d3275733b59b9975ad
Yolekoda <56722206+ilKogan@users.noreply.github.com>
2026-06-04 11:06:13 +0300

    Добавил позиции в Фото уровень

--- levels/level1/level1.gd
+++ levels/level1/level1.gd
@@ -1,15 +1,27 @@
 extends Node
 
 @onready var expected_pos = {
-	$JigsawFragment1.name: Vector2(0, 0),
-	$JigsawFragment2.name: Vector2(500, 300),
+	$JigsawFragment2.name: Vector2(766, 212),
+	$JigsawFragment3.name: Vector2(306, 198),
+	$JigsawFragment4.name: Vector2(1219,832),
+	$JigsawFragment5.name: Vector2(1509, 383),
+	$JigsawFragment6.name: Vector2(790, 547),
+	$JigsawFragment7.name: Vector2(1390, 547),
+	$JigsawFragment8.name: Vector2(239, 668),
+	$JigsawFragment9.name: Vector2(496, 404),
 }
 
 func _ready() -> void:
 	MessageManager.message_shown.connect(_on_message_shown)
 
-	$JigsawFragment1.drag_ended.connect(_on_jigsaw_placed)
 	$JigsawFragment2.drag_ended.connect(_on_jigsaw_placed)
+	$JigsawFragment3.drag_ended.connect(_on_jigsaw_placed)
+	$JigsawFragment4.drag_ended.connect(_on_jigsaw_placed)
+	$JigsawFragment5.drag_ended.connect(_on_jigsaw_placed)
+	$JigsawFragment6.drag_ended.connect(_on_jigsaw_placed)
+	$JigsawFragment7.drag_ended.connect(_on_jigsaw_placed)
+	$JigsawFragment8.drag_ended.connect(_on_jigsaw_placed)
+	$JigsawFragment9.drag_ended.connect(_on_jigsaw_placed)
 	LevelManager.level_changed.connect(_on_level_changed)
 
 
